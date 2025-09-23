bc15ff5bbe23a7a5f78614b7e729a022097c1159
Diana Belle <garbados@gmail.com>
2025-09-22 18:29:09 -0700

    checkin

--- src/the_emerald_green/web/templates/guides.cljs
+++ src/the_emerald_green/web/templates/guides.cljs
@@ -16,7 +16,7 @@
 (def introduction
   [:div
    [:div.block>h1.title.is-1>em "Welcome, mortal... Won't you play? Won't you dance?"]
-   [:div.block>div.columns>div.column.has-text-centered.is-three-quarters-desktop
+   [:div.block>div.columns>div.column.has-text-centered
     [:figure.image
      [:img
       {:src "./img/The_Emerald_Green.png"
